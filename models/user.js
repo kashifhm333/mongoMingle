25ddd522125cb64d9f5de3e7908696706c5d0239
kashifhm333 <Kashifjaanmemon786110@gmail.com>
2026-06-29 13:47:58 +0500

    sucessfull Image

--- models/user.js
+++ models/user.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mongoMingle");
+mongoose.connect(
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mongoMingle"
+)
+.then(() => console.log("✅ MongoDB Connected"))
+.catch((err) => console.error("❌ MongoDB Connection Error:", err));
 
-const userSchema = mongoose.Schema({
-name: String,
-email: String,
-image : String
-})
-
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  image: String
+});
 
 module.exports = mongoose.model("User", userSchema);
